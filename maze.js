2afb88b36ff778df8d0e42419809051f4cfef8d1
RMarc96 <salinasr@onid.oregonstate.edu>
2016-11-11 17:46:14 -0800

    Add files via upload

--- maze.js
+++ maze.js
@@ -1,1 +1,24 @@
-c
+var easyLink = document.getElementsByClassName("navbar-item")[0];
+var mediumLink = document.getElementsByClassName("navbar-item")[1];
+var hardLink = document.getElementsByClassName("navbar-item")[2];
+var instructionsLink = document.getElementsByClassName("navbar-item")[3];
+var title = document.querySelector("h1");
+
+
+
+function linkChange(event) {
+	alert("Link change!");
+}
+
+function titleClick(event) {
+	alert("You clicked the main header!");
+	
+}
+
+
+
+easyLink.addEventListener('click', linkChange);
+mediumLink.addEventListener('click', linkChange);
+hardLink.addEventListener('click', linkChange);
+instructionsLink.addEventListener('click', linkChange);
+title.addEventListener('click', titleClick);
