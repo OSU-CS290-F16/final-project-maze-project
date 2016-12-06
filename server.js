04e641d928d3446cf62761a45b6cafd599b85747
RMarc96 <salinasr@onid.oregonstate.edu>
2016-12-06 04:56:33 -0800

    Created an entirely new branch because I am paranoid

---
+++ server.js
@@ -0,0 +1,77 @@
+var path = require('path');
+var http = require("http");
+var fs = require('fs');
+
+var staticDir = __dirname;
+var port = process.env.PORT || 3000;
+
+/*
+var indexHTML= fs.readFileSync(staticDir + '/index.html','utf8');
+var indexMedHTML= fs.readFileSync(staticDir + '/indexMEDIUM.html','utf8');
+var indexHardHTML= fs.readFileSync(staticDir + '/indexHARD.html','utf8');
+var instructionsHTML= fs.readFileSync(staticDir + '/instructions.html','utf8');
+var errorHTML= fs.readFileSync(staticDir + '/404.html','utf8');
+var styleCSS= fs.readFileSync(staticDir + '/style.css','utf8');
+var styleCSS2= fs.readFileSync(staticDir + '/style2.css','utf8');
+var mazeJS= fs.readFileSync(staticDir + '/maze.js','utf8');
+*/
+var errorHTML= fs.readFileSync(staticDir +'/404.html','utf8');
+var instructionsHTML= fs.readFileSync(staticDir + '/instructions.html','utf8');
+
+// Some stuff from class examples
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var app = express();
+
+// Will store database access
+var mongoDB;
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
+
+// Parse all request bodies as JSON.
+app.use(bodyParser.json());
+
+// Serve static files from public/.
+app.use(express.static(__dirname));
+
+// Render the index page for the root URL path ('/').
+app.get('/', function (req, res) {
+  res.render('index-page', {
+    pageTitle: 'Welcome!'
+  });
+});
+
+app.get('/instructions', function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(instructionsHTML);
+    res.end();
+});
+
+app.get('*', function(req, res) {
+  res.writeHead(404, {"Content-Type": "text/html"});
+  res.write(errorHTML);
+  res.end();
+});
+
+function sendClearTime(endTimeInt) {
+	
+	console.log("Here is where '" + endTimeInt + "' will be sent to the database." );
+	
+}
+
+// Make a connection to Mongo database
+MongoClient.connect('mongodb://salinasr:usethisdb420@ds033317.mlab.com:33317/web_dev', function (err, db) {
+	if (err) {
+		console.log("== Unable to make connection to MongoDB Database.")
+		throw err;
+	}
+	mongoDB = db;
+	app.listen(port, function () {
+		console.log("== Listening on port", port);
+	});
+});
+
